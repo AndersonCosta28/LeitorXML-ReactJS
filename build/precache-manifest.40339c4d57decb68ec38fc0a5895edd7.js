1ec759bb79b0a6eb86f34a975d5bbfef871c9546
Mert1s <andgtsc@gmail.com>
2022-02-27 01:41:46 -0300

    Adicionado o express para rodar no Heroku

---
+++ build/precache-manifest.40339c4d57decb68ec38fc0a5895edd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c54f90ebc6bf2c126465287c55849d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47def0cc3533124cdc0d",
+    "url": "/static/css/main.aec91101.chunk.css"
+  },
+  {
+    "revision": "62bee190037210429985",
+    "url": "/static/js/2.f284695a.chunk.js"
+  },
+  {
+    "revision": "47def0cc3533124cdc0d",
+    "url": "/static/js/main.892b69fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
