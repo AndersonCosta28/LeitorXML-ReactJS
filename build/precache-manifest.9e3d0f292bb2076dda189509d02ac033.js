918867e39750aae1df7be75c0f4d879c8546c595
Mert1s <Andgtsc@Gmail.com>
2022-04-14 20:59:35 -0300

    build

---
+++ build/precache-manifest.9e3d0f292bb2076dda189509d02ac033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b03394305b53a848f1255671b88d5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3cb476423026219554",
+    "url": "/static/css/main.94f990f7.chunk.css"
+  },
+  {
+    "revision": "5dca8d88e2c054d1cf79",
+    "url": "/static/js/2.90c1e49d.chunk.js"
+  },
+  {
+    "revision": "1b3cb476423026219554",
+    "url": "/static/js/main.10044523.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
